109813753cee57e36b978d502cc26a9b45d50efb
ManishBallav <74704782+ManishBallav@users.noreply.github.com>
2021-11-13 15:26:39 +0530

    Update code.py

--- coiny-model1/code.py
+++ coiny-model1/code.py
@@ -1,21 +1,46 @@
+"""
+Coin detection using HoughCircles 
+"""
+
+
+"""
+Importing open CV, numpy and matplotlib
+"""
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-rupee = cv2.imread('images (1).jpg',1)
-from google.colab.patches import cv2_imshow
-cv2_imshow(rupee)
 
+
+"""
+Reading the image
+"""
+rupee1 = cv2.imread('images (1).jpg',1)
+from google.colab.patches import cv2_imshow
+cv2_imshow(rupee1)
+
+
+"""
+Converting the image to Grayscale
+"""
 img = cv2.cvtColor(rupee1, 
 		 cv2.COLOR_BGR2GRAY)
 plt.rcParams["figure.figsize"] = (16,9)
 plt.imshow(img,cmap='gray')
 
+
+"""
+Blurring the image using GaussianBlur
+"""
 img = cv2.GaussianBlur(img, 
 		      (21,21), 
 	              cv2.BORDER_DEFAULT)
 plt.rcParams["figure.figsize"] = (16,9)
 plt.imshow(img,cmap='gray')
 
+
+"""
+Finally applying HoughCircles on the preprocessed image
+"""
 circles = cv2.HoughCircles(img, 
 			   cv2.HOUGH_GRADIENT, 
                            0.9, 
@@ -36,3 +61,11 @@ for (x,y,r) in detected_circles[0,:]:
 	      2, 
 	     (0,255,255),
 	      3)
+
+
+"""
+Outputting the image(since the code is wriiten in google colab, 'google.colab.patches' library is used)
+"""
+from google.colab.patches import cv2_imshow
+plt.rcParams["figure.figsize"] = (16,9)
+cv2_imshow(rupee1)
